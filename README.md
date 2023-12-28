104940dd453b3bc9ccca4c1263a57d7859a5e909
kidus-der <101529458+kidus-der@users.noreply.github.com>
2023-12-28 08:13:06 -0700

    Framework for Neural Network and some explanantions to README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SVM_stock_price_predictor
-Stock price prediction program that uses different SVM(Support Vector Model) models to accurately predict the future stock price of a stock from past data.
+Stock price prediction program that uses different SVM(Support Vector Model) models (specifically RBF, Linear, and Polynomial  regression models) to accurately predict the future stock price of a stock from past data.
+
+Process:
+
+Retrieve Data for Training
+
+- Retrieve past stock price of a stock in a given time frame (I used 28 days) and collect that data into a csv file. I used Yahoo Finance for this, as you can set the timeframe you like and download a csv file straight from there.
+
+Reshape the data
+
+- use Numpy to reshape the array into a 2D array that we can then create regression models
+
+
+
---
+++ predictor.py
@@ -0,0 +1,73 @@
+import csv
+import numpy as np
+import sklearn.svm as sk
+import matplotlib.pyplot as plt
+from sklearn.svm import SVR
+
+def get_data(filename):
+
+    dates, prices = [], [] #initialize empty arrays for dates and prices
+
+    # open csv file, read, and append data
+    # to arrays
+    with open(filename, 'r') as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader) #skip first row (containing column descriptors)
+        for row in csvFileReader:
+            dates.append(int(row[0].split('-')[0])) #split the date string into int values, and append date to array
+            prices.append(float(row[1])) #append opening price to array
+
+    return dates, prices
+
+def train_models(dates, prices):
+
+    dates = np.reshape(dates, (len(dates), 1)) #reshape dates array to 2D array
+
+    # create our three models used to predict
+    linear_model = SVR(kernel = 'linear', c = 1e3)
+    polynomial_model = SVR(kernel = 'poly', c = 1e3, degree = 2) 
+    rbf_model = SVR(kernel = 'rbf', c = 1e3, gamma = 0.1)
+
+    # train our models using the '.fit' method using our two arrays for training
+    linear_model.fit(dates, prices)
+    polynomial_model.fit(dates, prices)
+    rbf_model.fit(dates, prices)
+
+    return linear_model, polynomial_model, rbf_model
+
+def plot_results(dates, prices, linear_model, polynomial_model, rbf_model):
+
+    plt.scatter(dates, prices, color = 'black', label = 'Data') # plot data points
+
+    # plot our regression models
+    plt.plot(dates, linear_model.predict(dates), color = 'red', label = 'Linear Model') 
+    plt.plot(dates, polynomial_model.predict(dates), color = 'green', label = 'Polynomial Model')
+    plt.plot(dates, rbf_model.predict(dates), color = 'blue', label = 'RBF Model')
+
+    # labes our axes
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+
+    # finalize our graph and produce plot
+    plt.title('Stock Price Prediction (SVR)')
+    plt.legend()
+    plt.show()
+
+def process_data(filename, x):
+    
+    # get our dates and prices from csv file
+    dates, prices = get_data(filename)
+
+    # train our models
+    linear_model, polynomial_model, rbf_model = train_models(dates, prices)
+
+    #plot our results
+    plot_results(dates, prices, linear_model, polynomial_model, rbf_model)
+
+    # prepare our predictions
+    x_reshape = np.reshape(x, (1, 1))
+    linear_prediction = linear_model.predict(x_reshape)[0]
+    polynomial_prediction = polynomial_model.predict(x_reshape)[0]
+    rbf_prediction = rbf_model.predict(x_reshape)[0]
+
+    return linear_prediction, polynomial_prediction, rbf_prediction
